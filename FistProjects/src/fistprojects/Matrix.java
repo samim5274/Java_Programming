c32209f676d335a6a1d359e03d128b132460ccaf
samim5274 <banglarcele1122@gmail.com>
2022-11-08 22:35:05 +0600

    update

--- FistProjects/src/fistprojects/Matrix.java
+++ FistProjects/src/fistprojects/Matrix.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fistprojects;
 
-/**
- *
- * @author SAMIM
- */
+import java.util.Scanner;
+
+
 public class Matrix {
+    
+    public static void main(String[] args) {
+        
+        Scanner objinput = new Scanner(System.in);
+        
+        
+        int A [][] = new int [2][3];
+        int B [][] = new int [2][3];
+        
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                A[row][col] = objinput.nextInt();
+            }
+        }
+        
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(" "+ A[row][col]);
+            }
+            System.out.println();
+        }
+        
+    }
     
 }
