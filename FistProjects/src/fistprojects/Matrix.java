404076eb3075403c2b5a76d0fc918e9d3814ec05
samim5274 <banglarcele1122@gmail.com>
2022-11-08 23:00:35 +0600

    update

--- FistProjects/src/fistprojects/Matrix.java
+++ FistProjects/src/fistprojects/Matrix.java
@@ -13,32 +13,52 @@ public class Matrix {
         
         int A [][] = new int [2][3];
         int B [][] = new int [2][3];
+        int C [][] = new int [2][3];
         
+        
+        System.out.println("Enter A matrix value. ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
+                System.out.printf(" A [%d][%d]=",row,col);
                 A[row][col] = objinput.nextInt();
             }
         }
         
+        System.out.println("Enter B matrix value. ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                System.out.print(" "+ A[row][col]);
-            }
-            System.out.println();
-        }
-        
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 3; col++) {
+                System.out.printf(" B [%d][%d]=",row,col);
                 B[row][col] = objinput.nextInt();
             }
         }
         
+        System.out.println("A = ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                System.out.print(" "+ B[row][col]);
+                System.out.print("\t "+ A[row][col]);
+            }
+            System.out.println();
+        }       
+        System.out.println();
+        
+        System.out.println("B = ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print("\t "+ B[row][col]);
             }
             System.out.println();
         }
+        System.out.println();
+        
+        System.out.println("A+B = ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                C[row][col] = (A[row][col]+ B[row][col]);
+                System.out.print("\t "+C[row][col]);
+            }
+            System.out.println();
+        }
+        System.out.println();
         
     }
     
